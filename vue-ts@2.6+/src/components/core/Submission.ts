472987638ad513d4272dcef8c0d83317795327d0
sitorhy <sitorhy@gmail.com>
2023-02-24 11:43:28 +0800

    [refactor]修改接口语义设计

--- vue-ts@2.6+/src/components/core/Submission.ts
+++ vue-ts@2.6+/src/components/core/Submission.ts
@@ -1,24 +1,24 @@
 import type {Rule, ValidateError} from "../core/Validator";
 
 export default interface Submission {
-    // 随机标识
+    // 随机标识，必须实现
     id: string;
 
-    // 窗口标识，选填，不确保有效性
+    // 窗口标识，选择实现，不确保有效性
     window?: string;
 
-    // 表单标识，必填
-    form: string | null;
+    // 关系标识，必须实现
+    relation: string | null;
 
-    // 字段标识，必填
-    name: string | Array<string | number> | null;
+    // 关系成员标识，非读写协议可以不实现
+    member?: string | Array<string | number> | null;
 
-    // 数据源值，主动事件可忽略改字段
+    // 成员值，一般与成员字段组合出现，只读和非读写协议可以不实现
     value?: any;
 
-    // 验证规则
+    // 验证规则，按情况选择实现，默认接口按 async-validator 标准设计
     rules?: Rule;
 
-    // 校验错误信息
+    // 校验错误信息，按情况选择实现
     errors?: ValidateError[];
 }
--- vue-ts@2.6+/src/components/vue/core/Events.ts
+++ vue-ts@2.6+/src/components/vue/core/Events.ts
@@ -1,31 +1,31 @@
 export default {
-    // 创建字段容器
+    // 创建成员容器
     REGISTER: "REGISTER",
 
-    // 挂起字段容器
+    // 挂起成员容器
     DISCONNECT: "DISCONNECT",
 
-    // 激活字段容器
+    // 激活成员容器
     CONNECT: "CONNECT",
 
-    // 激活离散结构的字段容器
+    // 激活离散结构的成员容器
     JOIN: "JOIN",
 
-    // 字段容器输入
-    INPUT: "INPUT",
+    // 成员容器输入
+    COLLECT: "COLLECT",
 
-    // 销毁字段容器
-    CANCEL: "CANCEL",
+    // 销毁成员容器
+    DEREGISTER: "DEREGISTER",
 
-    // 创建表单容器
+    // 创建关系容器
     BUILD: "BUILD",
 
-    // 激活表单容器
-    ONLINE: "ONLINE",
+    // 激活关系容器
+    MAINTAIN: "MAINTAIN",
 
-    // 挂起表单容器
-    OFFLINE: "OFFLINE",
+    // 挂起关系容器
+    FREEZE: "FREEZE",
 
-    // 销毁表单容器
-    DESTROY: "DESTROY"
+    // 销毁关系容器
+    RELEASE: "RELEASE"
 }
